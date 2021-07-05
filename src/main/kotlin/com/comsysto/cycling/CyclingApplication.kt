1c74316337ce5082f9e075ff79e81c9de1b4947e
Sebastijan Grabar <s.grabar@reply.de>
2021-07-05 13:35:53 +0200

    Initial commit

---
+++ src/main/kotlin/com/comsysto/cycling/CyclingApplication.kt
@@ -0,0 +1,12 @@
+package com.comsysto.cycling
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CyclingApplication
+
+fun main(args: Array<String>) {
+
+	runApplication<CyclingApplication>(*args)
+}
---
+++ src/test/kotlin/com/comsysto/cycling/CyclingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.comsysto.cycling
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CyclingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
